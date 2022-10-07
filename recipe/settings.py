c7dbbcfa486a79aab0baad9811f015fce5b16705
hoang.danh.quan <hoang.danh.quan@sun-asterisk.com>
2022-10-08 00:56:38 +0700

    next

--- recipe/settings.py
+++ recipe/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import dj_database_url
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ege*=!qe%bauj5hs0h(pj#jr1r#@9(ai*f7^^tj80yqo=p71zp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 STATICFILES_STORAGE = ''
 # Application definition
